6c9f76c542a1b732e92cbaa5c00bf90388ac1218
Axel Baudrillart <axbaudri@f2r9s12.clusters.42paris.fr>
2026-04-15 17:42:46 +0200

    from mysql to mysqld

--- srcs/requirements/mariadb/database.sh
+++ srcs/requirements/mariadb/database.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Initialiser le datadir si vide
+if [ ! -d /var/lib/mysql/mysql ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
 service mariadb start;
 
 mariadb -e  "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
