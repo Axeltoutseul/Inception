678154f40ff28174bd48ba3067d47bc9851ce5f7
Axel Baudrillart <axbaudri@f2r3s10.clusters.42paris.fr>
2026-04-16 15:29:44 +0200

    updated wordpress container

--- srcs/requirements/wordpress/setup_wordpress.sh
+++ srcs/requirements/wordpress/setup_wordpress.sh
@@ -10,11 +10,10 @@ if [ ! -f /var/www/html/wp-login.php ]; then
     cp /wp-config.php /var/www/html/wp-config.php
     wp core install \
         --path=/var/www/html \
-        --url="https://localhost:8443" \
+        --url="https://axbaudri.42.fr" \
         --title="Inception" \
         --admin_user="$WP_ADMIN" \
         --admin_password="$WP_ADMIN_PASSWORD" \
-        --admin_email="$WP_ADMIN_EMAIL" \
         --allow-root
 fi
 
