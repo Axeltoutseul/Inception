252d5bff79358872474a704e5d91daa4aec436e9
Axel Baudrillart <axbaudri@f2r4s10.clusters.42paris.fr>
2026-04-18 14:23:39 +0200

    two little changes

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_NAME', 'inception');
+define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOST', 'db:3306');
